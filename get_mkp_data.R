3813f98229ae4f14cac481c4cca0cad092213173
viniciusyl <vinicius.yl@gmail.com>
2022-01-04 17:27:54 -0300

    Ajuste de API

--- get_mkp_data.R
+++ get_mkp_data.R
@@ -4,5 +4,5 @@ library(tidyverse)
 
 # Get data ----
 
-## Dados de salário para o setores da indústria de transformação
-data_salario = get_sidra(api = "/t/1841/n1/all/v/all/p/all/C12762/all")s
+        ## Dados de salário para o setores da indústria de transformação
+        data_salario = get_sidra(api = "/t/1841/n1/all/v/all/p/all/C12762/all")
