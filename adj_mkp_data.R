6120aa2b140cce6eeba05372b5913fa044289021
viniciusyl <vinicius.yl@gmail.com>
2022-04-23 10:23:11 -0300

    Cruzamentos para geração do mark-up mais robusto. Baseado em left jons

--- adj_mkp_data.R
+++ adj_mkp_data.R
@@ -5,15 +5,33 @@ library(readr)
 library(tidyverse)
 
 # Get data ----
-data_receita = readr::read_csv2("https://raw.githubusercontent.com/viniciusyl/artigo_mkp/main/data_receita_ext.csv?token=GHSAT0AAAAAABT24GZDZCZ2WLQAR74OFSDSYTD5XIQ")
-data_custo = readr::read_csv2("https://raw.githubusercontent.com/viniciusyl/artigo_mkp/main/data_custo_ext.csv?token=GHSAT0AAAAAABT24GZCEXVUP2SEVUQ3RN4YYTD5WAQ")
-data_salario = readr::read_csv2("https://raw.githubusercontent.com/viniciusyl/artigo_mkp/main/data_salario_ext.csv?token=GHSAT0AAAAAABT24GZCGP6KX4V6A4TZK56IYTD5WWA")
+data_receita = readr::read_csv2("https://raw.githubusercontent.com/viniciusyl/artigo_mkp/main/data_receita_ext.csv")
+data_custo = readr::read_csv2("https://raw.githubusercontent.com/viniciusyl/artigo_mkp/main/data_custo_ext.csv")
+data_salario = readr::read_csv2("https://raw.githubusercontent.com/viniciusyl/artigo_mkp/main/data_salario_ext.csv")
 
-# Criar valor mkp e associar a setor CNAE 2.0
-mkp = data_receita[data_receita$Variável == "Receita líquida de vendas", "Valor"] / (data_custo[data_custo$Variável == "Custo direto de produção - Total", "Valor"] + data_salario[data_salario$Variável == "Salários, retiradas e outras remunerações de pessoal assalariado ligado à produção", "Valor"])
-data_mkp = cbind.data.frame(data_receita[data_receita$Variável == "Receita líquida de vendas", c("Ano", "Classificação Nacional de Atividades Econômicas (CNAE 2.0)")], mkp)
+# Criar valor mkp e associar a setor CNAE 2.0 ----
 
-# Identificar agrupamento de setores 
+        # Criar link entre dataframes
+        data_receita$link = str_c(data_receita$Ano, "_", data_receita$`Classificação Nacional de Atividades Econômicas (CNAE 2.0)`)
+        data_custo$link = str_c(data_custo$Ano, "_", data_custo$`Classificação Nacional de Atividades Econômicas (CNAE 2.0)`)
+        data_salario$link = str_c(data_salario$Ano, "_", data_salario$`Classificação Nacional de Atividades Econômicas (CNAE 2.0)`)
+
+        # Criar data mkp         
+        data_mkp = unique(data_receita[c("Ano", "Classificação Nacional de Atividades Econômicas (CNAE 2.0)")])
+        data_mkp$link = str_c(data_mkp$Ano, "_", data_mkp$`Classificação Nacional de Atividades Econômicas (CNAE 2.0)`)
+        
+        # Unir informações
+        data_mkp = left_join(data_mkp, data_receita[data_receita$Variável == "Receita líquida de vendas", c("Valor", "link")], by = "link")
+        names(data_mkp)[4] = "Receita líquida de vendas"
+        data_mkp = left_join(data_mkp, data_custo[data_custo$Variável == "Custo direto de produção - Total", c("Valor", "link")], by = "link")
+        names(data_mkp)[5] = "Custo direto de produção - Total"
+        data_mkp = left_join(data_mkp, data_salario[data_salario$Variável == "Salários, retiradas e outras remunerações de pessoal assalariado ligado à produção", c("Valor", "link")], by = "link")
+        names(data_mkp)[6] = "Salários, retiradas e outras remunerações de pessoal assalariado ligado à produção"
+        
+        # Calcular mark-up
+        data_mkp$mkp = data_mkp$`Receita líquida de vendas` / (data_mkp$`Custo direto de produção - Total` + data_mkp$`Salários, retiradas e outras remunerações de pessoal assalariado ligado à produção`)
+
+# Identificar agrupamento de setores ----
 data_mkp[str_detect(data_mkp$`Classificação Nacional de Atividades Econômicas (CNAE 2.0)`, "[0-9]+[0-9][[:space:]]"), ]
 
 
